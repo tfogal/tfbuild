f8ff600ba303012eee5518f58163ae4b71ce806f
Tom Fogal <tfogal@alumni.unh.edu>
2009-01-31 12:53:09 -0700

    `Initial' commit.
    Unfortunately this has been under development for quite a while
    without version control...

---
+++ bf.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+# Build functionality
+
+PREFIX="/scratch/sw"
+JOBS=4
+
+function download()
+{
+   if [ -z "$@" ]; then
+      echo "Download _what_?"
+      exit 1;
+   fi
+   wget -nv "$@"
+}
+function src_download()
+{
+	if [ -z "${URL}" ]; then
+		echo "URL unset!  What are we dling?";
+		exit 1;
+	fi
+   download "${URL}"
+}
+
+function src_extract()
+{
+    # chop off the common extension.  If it changes, thats the right type.
+	if [ "${TARBALL%%bz2}" != "${TARBALL}" ] ; then
+        # Bzip2.
+		tar jxf "${TARBALL}"
+	elif [ "${TARBALL%%gz}" != "${TARBALL}" ] ; then
+		# gzip
+		tar zxf "${TARBALL}"
+    elif [ "${TARBALL%%zip}" != "${TARBALL}" ] ; then
+        # zip
+        unzip "${TARBALL}"
+    else
+        echo "Unknown file type!"
+        exit 1
+	fi
+}
+
+function src_configure()
+{
+	./configure $@ --prefix="${PREFIX}"
+	if [ $? != 0 ]; then
+		echo "Configuration failed!  Bailing out..."
+		exit 1
+	fi
+}
+
+function src_make()
+{
+	# don't add jobs option if not set
+	if [ -z "${JOBS}" ]; then
+		nice -n 15 make $@
+	else
+		nice -n 15 make -j${JOBS} $@
+	fi
+}
+
+function src_install()
+{
+	make install $@ || exit 1
+}
+
+function setup()
+{
+	mkdir -p ${PREFIX}/tmp/work
+	pushd ${PREFIX}/tmp/work
+}
+function teardown()
+{
+	popd
+	rm -fr ${PREFIX}/tmp/work
+}
+
+function src_cd()
+{
+   # chop off the common extension.  If it changes, thats the right type.
+   if [[ -n "${DIR}" ]] ; then
+      cd "${DIR}" || exit 1
+   elif [ "${TARBALL%%bz2}" != "${TARBALL}" ] ; then
+      cd "${TARBALL%%.tar.bz2}" || exit 1
+   elif [ "${TARBALL%%tar.gz}" != "${TARBALL}" ] ; then
+      cd "${TARBALL%%.tar.gz}" || exit 1
+	elif [ "${TARBALL%%tgz}" != "${TARBALL}" ] ; then
+      cd "${TARBALL%%.tgz}" || exit 1
+   elif [ "${TARBALL%%zip}" != "${TARBALL}" ] ; then
+      cd "${TARBALL%%.zip}" || exit 1
+   else
+      cd "${TARBALL}" || exit 1
+   fi
+}
+
+function setup_env()
+{
+    # Add our custom path to the build configuration flags.
+    export CFLAGS="${CFLAGS} -m64 -fPIC -I${PREFIX}/include"
+    export CXXFLAGS="${CXXFLAGS} -m64 -fPIC -I${PREFIX}/include"
+    export LDFLAGS="${LDFLAGS} -m64 -L${PREFIX}/lib64 -L${PREFIX}/lib"
+}
+
+function gnu_pkg()
+{
+    setup
+    src_download
+    src_extract
+    src_cd
+
+    setup_env
+    src_configure --disable-dependency-tracking $@
+    src_make
+    src_install
+    teardown
+}
+
+function python_pkg()
+{
+    setup
+    src_download
+    src_extract
+    src_cd
+
+    setup_env
+    python setup.py build || exit 1
+    python setup.py build_clib --prefix=${PREFIX} || exit 1
+    python setup.py install --prefix=${PREFIX} || exit 1
+    teardown
+}
---
+++ env_additions.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# sh-style script to setup environment for tfbuild.
+
+# pick up PREFIX:
+. ${HOME}/build/bf.sh
+
+PATH="${PREFIX}/bin:$PATH"
+
+ACLOCAL="aclocal -I ${PREFIX}/share/aclocal"
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib64"
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
+MANPATH="${MANPATH}:${PREFIX}/man:${PREFIX}/share/man"
+PERL5LIB="${PREFIX}/lib/perl5/site_perl:${PERL5LIB}"
+PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+PYTHONPATH="${PYTHONPATH}:${PREFIX}/lib64/python2.5/site-packages"
+PYTHONPATH="${PYTHONPATH}:${PREFIX}/lib/python2.5/site-packages"
+TEXINPUTS="${TEXINPUTS}:${PREFIX}/tftex"
+
+# ugh.  do some hackery to remove duplicates from our path specifications.
+# This happens when using programs like screen; the shell we logged into adds a
+# path, starting an xterm would add paths, and then each shell-in-a-screen is a
+# login shell, so would also add it's own paths.
+function fixpath {
+    # we need to separate the paths by newlines for sort and uniq to work
+    pathlist=$(echo "${@}" | tr : \\n)
+
+    unique=`echo -e "${pathlist}" | sort | uniq`
+
+    # we can end up with a blank line after the sort, sometimes.  it will
+    # always be the first line, so if the first line is a path we're already
+    # all set -- otherwise get rid of the first couple bytes.
+    ln1=`echo -e "${unique}" | head --bytes=1`
+    if test "x${ln1}" = "x/" ; then
+        # don't strip them -- it's a path!
+        v=`echo -e "${unique}" | tr "\n" ":"`
+    else
+        v=`echo -e "${unique}" | tail --bytes=+2 | tr "\n" ":"`
+    fi
+    echo ${v}
+}
+
+LD_LIBRARY_PATH=$(fixpath "${LD_LIBRARY_PATH}")
+MANPATH=$(fixpath "${MANPATH}")
+# fixpath fails for paths that are already good -- without redundant ":"'s,
+# etc.
+# PATH=$(fixpath "${PATH}")
+PKG_CONFIG_PATH=$(fixpath "${PKG_CONFIG_PATH}")
+PERL5LIB=$(fixpath "${PERL5LIB}")
+PYTHONPATH=$(fixpath "${PYTHONPATH}")
+TEXINPUTS=$(fixpath "${TEXINPUTS}")
+
+export ACLOCAL
+export LD_LIBRARY_PATH
+export MANPATH
+export PATH
+export PERL5LIB
+export PKG_CONFIG_PATH
+export PYTHONPATH
+export TEXINPUTS
